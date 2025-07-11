cbf8ee312013d612173cf8eef455ba4933dbff4f
Kuulbudam Trisma <86109372+Kuulbudam@users.noreply.github.com>
2025-07-11 19:48:42 +0100

    Add files via upload
    Optimized FundMe.sol for gas efficiency

--- FundeMe.sol
+++ FundeMe.sol
@@ -5,18 +5,26 @@
 // sending Eth through a function and revert
 pragma solidity ^0.8.0;
 import"@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+error notOwner();
 contract FundMe {
-uint256 public minimumUsd=50 * 1e18;
+uint256 public  constant MINIMUM_USD=50 * 1e18;
 address[] public funders;
 mapping (address=>uint256) public addressToAMountFunded;
+ 
+ address public immutable I_owner;
+      constructor(){
+       I_owner=msg.sender;
+      }
 
 function fund () public payable {
     // Want to be able to set a minimum fund amount in USD
     // How do we send ETH to this contract
     
-    require(msg.value >= minimumUsd, "Didn't send enough ethereum");// 1e18 ==1*10 ^18==1 ETH
+    //require(getConversionRate(msg.value) >= MINIMUM_USD, "Didn't send enough ethereum");// 1e18 ==1*10 ^18==1 ETH
+    if(msg.sender !=I_owner) {revert notOwner();}
     funders.push(msg.sender);
     addressToAMountFunded[msg.sender] = msg.value;
+    
     }
 function getPrice()public view returns (uint256) {
 //ABI
@@ -32,6 +40,8 @@ uint256 ethAmountInUsd = (ethAmount * ethPrice)/1e18;
 return ethAmountInUsd ;
 }
 function withdraw()  public {
+    //require(msg.sender == I_owner,"sender is not owner");
+    if(msg.sender !=I_owner) {revert notOwner();}
     //starting index, ending index, stop amount
     for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
         //code
@@ -45,4 +55,11 @@ funders= new address[](0); // resetting the array
 (bool callSuccess, )= payable (msg.sender).call{value: address(this).balance}("");
 require(callSuccess,"Call Fail");
   }
+  //what happens when someone send this contract ETH withouthout calling the fund function
+  receive() external payable {
+    fund();
+   }
+   fallback() external payable { 
+    fund();
+   }
 } 
